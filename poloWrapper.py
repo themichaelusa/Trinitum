095a6cfc46034bd0a2baf3c7a94bb66aa07ac5b5
Shovik Guha <shovik.guha@gmail.com>
2017-05-17 10:46:04 -0700

    added wrapper for Poloniex API

---
+++ poloWrapper.py
@@ -0,0 +1,97 @@
+import urllib
+import urllib.request as urllib2
+import json
+import time
+import hmac, hashlib
+
+
+def createTimeStamp(datestr, format="%Y-%m-%d %H:%M:%S"):
+    return time.mktime(time.strptime(datestr, format))
+
+
+class poloniex:
+    def __init__(self, APIKey, Secret):
+        self.APIKey = APIKey
+        self.Secret = Secret
+
+    def post_process(self, before):
+        after = before
+
+        # Add timestamps if there isnt one but is a datetime
+        if ('return' in after):
+            if (isinstance(after['return'], list)):
+                for x in range(0, len(after['return'])):
+                    if (isinstance(after['return'][x], dict)):
+                        if ('datetime' in after['return'][x] and 'timestamp' not in after['return'][x]):
+                            after['return'][x]['timestamp'] = float(createTimeStamp(after['return'][x]['datetime']))
+
+        return after
+
+    def api_query(self, command, req={}):
+
+        if (command == "returnTicker" or command == "return24Volume"):
+            ret = urllib2.urlopen(urllib2.Request('https://poloniex.com/public?command=' + command))
+            return json.loads(ret.read())
+        elif (command == "returnOrderBook"):
+            ret = urllib2.urlopen(urllib2.Request(
+                'https://poloniex.com/public?command=' + command + '&currencyPair=' + str(req['currencyPair'])))
+            return json.loads(ret.read())
+        elif (command == "returnMarketTradeHistory"):
+            ret = urllib2.urlopen(urllib2.Request(
+                'https://poloniex.com/public?command=' + "returnTradeHistory" + '&currencyPair=' + str(
+                    req['currencyPair'])))
+            return json.loads(ret.read())
+        elif (command == "returnChartData"):
+            ret = urllib2.urlopen(urllib2.Request(
+                'https://poloniex.com/public?command=returnChartData&currencyPair=' + str(
+                    req['currencyPair']) + '&start=' + str(req['start']) + '&end=' + str(req['end']) + '&period=' + str(
+                    req['period'])))
+            return json.loads(ret.read())
+        else:
+            req['command'] = command
+            req['nonce'] = int(time.time() * 1000)
+            post_data = urllib.urlencode(req)
+
+            sign = hmac.new(self.Secret, post_data, hashlib.sha512).hexdigest()
+            headers = {
+                'Sign': sign,
+                'Key': self.APIKey
+            }
+
+            ret = urllib2.urlopen(urllib2.Request('https://poloniex.com/tradingApi', post_data, headers))
+            jsonRet = json.loads(ret.read())
+            return self.post_process(jsonRet)
+
+    def return24Volume(self):
+        return self.api_query("return24Volume")
+
+    def returnTicker(self):
+        return self.api_query("returnTicker")
+
+    def returnOrderBook(self, currencyPair):
+        return self.api_query("returnOrderBook", {'currencyPair': currencyPair})
+
+    def returnMarketTradeHistory(self, currencyPair):
+        return self.api_query("returnMarketTradeHistory", {'currencyPair': currencyPair})
+
+    def cancel(self, currencyPair, orderNumber):
+        return self.api_query('cancelOrder', {"currencyPair": currencyPair, "orderNumber": orderNumber})
+
+    def buy(self, currencyPair, rate, amount):
+        return self.api_query('buy', {"currencyPair": currencyPair, "rate": rate, "amount": amount})
+
+    def returnOpenOrders(self, currencyPair):
+        return self.api_query('returnOpenOrders', {"currencyPair": currencyPair})
+
+    def returnTradeHistory(self, currencyPair):
+        return self.api_query('returnTradeHistory', {"currencyPair": currencyPair})
+
+    def returnBalances(self):
+        return self.api_query('returnBalances')
+
+    def sell(self, currencyPair, rate, amount):
+        return self.api_query('sell', {"currencyPair": currencyPair, "rate": rate, "amount": amount})
+
+
+    def withdraw(self, currency, amount, address):
+        return self.api_query('withdraw', {"currency": currency, "amount": amount, "address": address})
